51c20e84b273d489d40792e0deaaa105359e1ac0
Namtc2 <126969945+Namtc2@users.noreply.github.com>
2023-03-06 00:10:19 +0700

    Hoàn thành giao diện WPF cho các chức năng

--- WarehouseManegement/ViewModel/MainViewModel.cs
+++ WarehouseManegement/ViewModel/MainViewModel.cs
@@ -12,6 +12,13 @@ namespace WarehouseManegement.ViewModel
     {
         public bool isLoaded = false;
         public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SuplierCommand { get; set; }
+        public ICommand CustomersCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
         public MainViewModel()
         {
             LoadedWindowCommand = new RelayCommand<Window>((p) => { return p != null ? true : false; }, (p) =>
@@ -20,6 +27,48 @@ namespace WarehouseManegement.ViewModel
                 LoginWindow loginWd = new LoginWindow();
                 loginWd.Topmost = true;
                 loginWd.ShowDialog();
+            });
+            UnitCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                UnitWindow unitWd = new UnitWindow();
+                unitWd.Topmost = true;
+                unitWd.ShowDialog();
+            });
+            SuplierCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow unitWd = new SuplierWindow();
+                unitWd.Topmost = true;
+                unitWd.ShowDialog();
+            });
+            CustomersCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                CustomersWindow cusWd = new CustomersWindow();
+                cusWd.Topmost = true;
+                cusWd.ShowDialog();
+            });
+            ObjectCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow obWd = new ObjectWindow();
+                obWd.Topmost = true;
+                obWd.ShowDialog();
+            });
+            UserCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                UserWindow obWd = new UserWindow();
+                obWd.Topmost = true;
+                obWd.ShowDialog();
+            });
+            InputCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                InputWindow inputWd = new InputWindow();
+                inputWd.Topmost = true;
+                inputWd.ShowDialog();
+            });
+            OutputCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                OutputWindow opWd = new OutputWindow();
+                opWd.Topmost = true;
+                opWd.ShowDialog();
             });
         }        
     }
